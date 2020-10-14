b7b8f93939dcb7fb0b84e014ad6ea39c3a929595
Robin ZMUDA <robinzmuda@gmail.com>
2020-10-14 10:31:06 +0200

    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+<head>
+  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+  <title>C'est sur internet donc c'est vrai</title>
+</head>
+<h1>Les limites de vitesse sont augmentes de 100 km/h supplementaires si la personne est sous l'effet de Cannabis</h1>
+<h1>Esteban est une grosse salope</h1>
+<h1> Les chinois ils sont tous moches c'est des versions ratées de nous</h1>
+
+<canvas id="canvas"></canvas>
+  <script  src="./script.js"></script>
+
+</body>
+</html>
