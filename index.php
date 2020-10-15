2e0711a06d4dc767d756967137b0a1552a834297
Anhydrite <robinzmuda@gmail.com>
2020-10-15 10:54:34 +0200

    bite

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Les lois d'internet</title>
+  <link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div id="WebGL-output">
+</div>
+
+<div class="side-menu">
+  <div id="menuBtn">
+    <input type="checkbox" />
+    <span></span>
+    <span></span>
+  </div>
+  <div class="menu">
+    <a class="menu-item" href="#">NIQUE</a>
+    <a class="menu-item" href="#">TA</a>
+    <a class="menu-item" href="#">GROSSE</a>
+    <a class="menu-item" href="#">MERE</a>
+  </div>
+  <div class="straight-line"></div>
+</div>
+
+<img class="planetImg" src="https://assets.codepen.io/1651485/planet.png" />
+
+<div class="mainText">
+  <div class="mainTitle">
+    Les lois d'internet
+  </div>
+  <div class="subTitle">
+    <p>Les limites de vitesse sont augmentes de 100 km/h supplementaires si la personne est sous l'effet de Cannabis</p>
+    <p>Esteban est une grosse salope</p>
+    <p>Les chinois ils sont tous moches c'est des versions ratées de nous</p>
+    <p>Si mon fils il est trizo il finira dans le frigo</p>
+    <p>Pas de quartier pour les pd</p>
+  </div>
+
+  
+
+  <div class="cursor"></div>
+</div>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js'></script>
+<script src='https:////cdnjs.cloudflare.com/ajax/libs/dat-gui/0.5/dat.gui.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/three.js/r70/three.min.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
