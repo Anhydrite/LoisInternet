5ce63490d587b8f3eb5718d74ee56892bc4972a0
Anhydrite <49342114+Anhydrite@users.noreply.github.com>
2021-02-03 21:11:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
+<script>
 $.get('https://www.cloudflare.com/cdn-cgi/trace', function(data) {
     console.log(data)
  https://bonjour.free.beeceptor.com
 })
+</script>
